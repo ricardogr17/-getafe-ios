3af9899190d1a431b32178489ec7cc5f32fd2af4
RicadoGR <Bowser.riki@gmail.com>
2019-09-20 19:28:49 +0200

    Init project

---
+++ getafeios/getafeios/Controllers/ViewControllers.swift
@@ -0,0 +1,9 @@
+//
+//  ViewControllers.swift
+//  getafeios
+//
+//  Created by Dev2 on 20/09/2019.
+//  Copyright © 2019 Dev. All rights reserved.
+//
+
+import Foundation
---
+++ getafeios/getafeios/Data/Local/Models/Student.swift
@@ -0,0 +1,9 @@
+//
+//  Student.swift
+//  getafeios
+//
+//  Created by Dev2 on 20/09/2019.
+//  Copyright © 2019 Dev. All rights reserved.
+//
+
+import Foundation
